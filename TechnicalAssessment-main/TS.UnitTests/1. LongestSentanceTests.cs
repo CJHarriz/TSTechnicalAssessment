7640353bc1b440b69d3492d708740f149c62a0a3
CJHarriz <60986970+CJHarriz@users.noreply.github.com>
2024-09-06 19:31:41 +0200

    Added requested test case to LongestSentanceTests
    Requested test for input "Forget CVs..Save time . x x" has been added and is succeeding.

--- TechnicalAssessment-main/TS.UnitTests/1. LongestSentanceTests.cs
+++ TechnicalAssessment-main/TS.UnitTests/1. LongestSentanceTests.cs
@@ -8,9 +8,23 @@ public class LongestSentanceTests
     [TestMethod]
     public void Q1_BasicNominalTest()
     {
-        var input = $"We test coders. Give us a try";
+        var input = "We test coders. Give us a try";
         var outcome = LongestSentanceAnswer.Solution(input);
 
         Assert.AreEqual(4, outcome);
+    }
+
+    // The requested test
+    [TestMethod]
+    public void Q2_TestWithMultipleDelimetersAndSpaces()
+    {
+        // Arrange
+        var input = "“Forget CVs..Save time . x x";
+
+        // Act
+        var outcome = LongestSentanceAnswer.Solution(input);
+
+        // Assert
+        Assert.AreEqual(2, outcome);
     }
 }
