11a939aeb861ea3b9808faf482653d6070040232
CJHarriz <60986970+CJHarriz@users.noreply.github.com>
2024-09-06 19:14:17 +0200

    Brought instructions onto a footnote
    Just helping me read the requirements

--- TechnicalAssessment-main/TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TechnicalAssessment-main/TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace TS.TechnicalTest;
+﻿namespace TS.TechnicalTest;
 
 public class LongestSentanceAnswer
 {
@@ -8,3 +7,17 @@ public class LongestSentanceAnswer
         throw new NotImplementedException("Not completed yet");
     }
 }
+
+#region Footnotes
+//Requirements
+
+//Find the sentence containing the largest number of words in the given text and count the number of
+//words it contains.
+//• The text is specified as a string S consisting of N characters: letters, spaces, dots(.), question
+//marks(?) and exclamation marks(!).
+//• A sentence can be divided into words by splitting it at spaces.A sentence without words is
+//valid, but a valid word must contain at least one letter.
+//• The length of S is within the range[1..100];
+//• String S consists only of letters(a−z, A−Z), spaces, dots(.), question marks(?) and
+//exclamation marks(!).
+#endregion
